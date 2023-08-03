1d26479fc521e3741241ab73d06d47612bd0bef2
liamlutton <lutton+gh@me.com>
2023-08-03 13:06:05 -0700

    Try to setup project

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+type TextObject = TextLabel | TextBox;
+
+export type HighlightProps = {
+    textObject: TextObject,
+	src?: string,
+	forceUpdate?: boolean,
+	customLang?: { [str: string]: string },
+}
+
+export namespace Highlighter {
+	/**
+	 * Highlights the given textObject with the given props and returns a cleanup function. Highlighting will automatically update when needed, so the cleanup function will disconnect those connections and remove all labels.
+	 */
+    function highlight(props: HighlightProps): () => void;
+}
